44c079d1bfc3b4c56676424a68e465ecbda2964e
aki <kom88aki@yahoo.co.jp>
2016-12-26 22:29:18 +0900

    ScriptTest

---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+		int[] a = { 1, 2, 3, 4, 5 };
+		for(int i = 0; i < a.Length; i++) {
+			Debug.Log(a[4 - i]);
+				}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
